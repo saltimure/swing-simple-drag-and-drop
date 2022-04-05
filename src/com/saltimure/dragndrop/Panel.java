1e4e63cce9a824d028ccc24f7ff8a879c4ad7d9e
TImur Salekhov <saltimure@gmail.com>
2022-04-05 19:36:16 +0300

    create jpanel

---
+++ src/com/saltimure/dragndrop/Panel.java
@@ -0,0 +1,14 @@
+package com.saltimure.dragndrop;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Panel extends JPanel {
+   
+    public Panel() {
+        this.setBackground(Color.BLACK);
+        this.setSize(100, 100);
+    }
+}
